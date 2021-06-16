2d091b5da7289e154e0aea034a9751f46edd9713
saurabh <saurabh.sharma@ncr.com>
2021-06-16 06:39:03 -0700

    palindrome

---
+++ python/leetcode/easy/palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x <0:
+            return False
+        else:
+            reverse_integer=int(str(x)[::-1])
+            if x== reverse_integer:
+                return True
+            else:
+                return False
+        
+
+
+d = Solution()
+check=d.isPalindrome(121)
+print(check)
