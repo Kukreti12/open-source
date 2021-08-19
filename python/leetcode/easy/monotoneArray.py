251a21b3046936a8069c98e771e190bbc482d26c
saurabh <saurabh.sharma@ncr.com>
2021-08-18 19:22:01 -0700

    Revert "update"
    This reverts commit 4d9f557983a3b710293a7cab1ae576c1d89606b3.

--- python/leetcode/easy/monotoneArray.py
+++
@@ -1,5 +0,0 @@
-class Solution(object):
-    def isMonotonic(self, A):
-        return (all(A[i] <= A[i+1] for i in range(len(A) - 1)) or  # all will check all the elements in tuple or list as true
-                all(A[i] >= A[i+1] for i in range(len(A) - 1)))
-
