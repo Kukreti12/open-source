178798203ea3a0b7f910a7021ab222e79701ec6f
saurabh <saurabh.sharma@ncr.com>
2021-08-20 11:45:11 -0700

    add

--- janus/gremlin.py
+++ janus/gremlin.py
@@ -3,6 +3,7 @@ from gremlin_python.structure.graph import Graph
 from gremlin_python.process.graph_traversal import __
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 
+#intitial
 graph = Graph()
 connection = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
 # The connection should be closed on shut down to close open connections with connection.close()
