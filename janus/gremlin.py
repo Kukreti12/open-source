bd67c51ef68cb86a6daca92df85f96d6b8037763
saurabh <saurabh.sharma@ncr.com>
2021-08-20 11:51:57 -0700

    test2

--- janus/gremlin.py
+++ janus/gremlin.py
@@ -9,5 +9,6 @@ connection = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
 g = graph.traversal().withRemote(connection)
 # Reuse 'g' across the application
 #test
+#test2
 marko = g.V().has('person','name','marko').next()
 peopleMarkoKnows = g.V().has('person','name','marko').out('knows').toList()
