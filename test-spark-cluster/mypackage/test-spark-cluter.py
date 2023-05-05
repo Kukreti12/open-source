7a40c2196dfb5df32027254046dba4c716ed2645
saurabh sharma <saurabh.sharma@hpe.com>
2023-05-05 12:10:17 -0700

    spark commit

---
+++ test-spark-cluster/mypackage/test-spark-cluter.py
@@ -0,0 +1,25 @@
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+sc = SparkContext()
+spark = SparkSession(sc)
+
+
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+data2 = [("James","","Smith","36636","M",3000),
+    ("Michael","Rose","","40288","M",4000),
+    ("Robert","","Williams","42114","M",4000),
+    ("Maria","Anne","Jones","39192","F",4000),
+    ("Jen","Mary","Brown","","F",-1)
+  ]
+
+schema = StructType([ \
+    StructField("firstname",StringType(),True), \
+    StructField("middlename",StringType(),True), \
+    StructField("lastname",StringType(),True), \
+    StructField("id", StringType(), True), \
+    StructField("gender", StringType(), True), \
+    StructField("salary", IntegerType(), True) \
+  ])
+ 
+df = spark.createDataFrame(data=data2,schema=schema)
+df.show(truncate=False)
