06846ed0cdbd59b3d5cb34a23e1daa1f64079767
saurabh sharma <saurabh.sharma@hpe.com>
2023-06-09 14:31:18 -0700

    update dag with s3 cred with no verification

--- airflow/example_dags/app.py
+++ airflow/example_dags/app.py
@@ -39,10 +39,10 @@ def download_rates():
 
 def s3_upload():
     s3_resource = boto3.resource('s3',
-                        endpoint_url='https://singlenodedf.ailab.local:9000',
+                        endpoint_url='https://10.10.162.46:9000',
                         aws_access_key_id='7GM4BT71Q56BX55FZOCN4XF4ATVQTS0TJIPTVJIPC82KAFZLVGC5Q2ELQIDAJEORM6FWOYSL7D40ZZQC1PJZJANHXSL3LNIRAKGVM3EY2NDYAXPX3AFXS0A6F',
                         aws_secret_access_key='BNARYBWC1LSDK02S7O8VQRPHHXTD78Q6K52UZZ5XMRCXZX8K202CNE1VWTXGX4OGJ6T2WNMUX',
-                        verify= '/mnt/shared/df5.pem')
+                        verify= False)
 
     # download the object 'your_file.gz' from the bucket 'your_bucket' and save it to local FS as your_file_downloaded.gz
     #s3.Bucket('your_bucket').download_file('your_directory/your_file.gz', 'your_file_downloaded.gz')
