6b132dcecc790d4240bd6dde4c4a247d5800064a
saurabh sharma <saurabh.sharma@hpe.com>
2023-06-12 16:09:38 -0700

    add the CI changes

--- airflow/example_dags/test.py
+++ airflow/example_dags/test.py
@@ -17,7 +17,7 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
-def get_data_postgres():
+def get_data_postgres(**context):
      # Connect to PostgreSQL using PostgresHook
     postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
     result = postgres_hook.get_records("SELECT * FROM nyc")
@@ -27,8 +27,7 @@ def get_data_postgres():
         key = row[0]  # Assuming the first column is the key
         value = row[1]  # Assuming the second column is the value
         data_dict[key] = value
-        task_instance = XCom.get_current_task_instance()
-        task_instance.xcom_push(key=key, value=value)
+        context['ti'].xcom_push(key=key, value=value)
 
 with DAG("get_metadata", start_date=datetime(2023, 3 ,14), 
     schedule_interval="@daily",
@@ -37,7 +36,7 @@ with DAG("get_metadata", start_date=datetime(2023, 3 ,14),
 
     nyc_get_audit_data = PythonOperator(
             task_id="nyc_get_audit_data",
-            python_callable=get_data_postgres, do_xcom_push=True
+            python_callable=get_data_postgres, provide_context=True
     )
         
 
