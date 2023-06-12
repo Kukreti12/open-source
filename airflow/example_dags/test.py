59817f8972a8f51fb2c18cf7ec17e8e0434fc8ba
saurabh sharma <saurabh.sharma@hpe.com>
2023-06-12 16:43:41 -0700

    update timestamp

--- airflow/example_dags/test.py
+++ airflow/example_dags/test.py
@@ -20,13 +20,14 @@ default_args = {
 def get_data_postgres():
      # Connect to PostgreSQL using PostgresHook
     postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
-    result = postgres_hook.get_records("SELECT * FROM nyc")
+    result = postgres_hook.get_records("SELECT taxitype, EXTRACT(YEAR FROM lastmonthprocessed) as year, EXTRACT(MONTH FROM lastmonthprocessed) as month FROM nyc")
     # Process the result and save it in a dictionary
     data_dict = {}
     for row in result:
         key = row[0]  # Assuming the first column is the key
-        value = row[1]  # Assuming the second column is the value
-        data_dict[key] = value
+        year = row[1]
+        month = row[2]  # Assuming the second column is the value
+        data_dict[key] = year+"-"+month
         # context['ti'].xcom_push(key=key, value=value)
     return data_dict
 with DAG("get_metadata", start_date=datetime(2023, 3 ,14), 
