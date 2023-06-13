e9c31d82f7f91d4eaa5bbe7aff321ddb8a3441e5
saurabh sharma <saurabh.sharma@hpe.com>
2023-06-12 21:36:43 -0700

    check the output from postgres table

--- airflow/example_dags/test.py
+++ airflow/example_dags/test.py
@@ -14,31 +14,42 @@ default_args = {
     "email_on_retry": False,
     "email": "admin@localhost.com",
     "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    "retry_delay": timedelta(minutes=5),
 }
 
+
 def get_data_postgres():
-     # Connect to PostgreSQL using PostgresHook
-    postgres_hook = PostgresHook(postgres_conn_id='postgres_default')
-    result = postgres_hook.get_records("SELECT taxitype, EXTRACT(YEAR FROM lastmonthprocessed) as year, EXTRACT(MONTH FROM lastmonthprocessed) as month FROM nyc")
+    # Connect to PostgreSQL using PostgresHook
+    postgres_hook = PostgresHook(postgres_conn_id="postgres_default")
+    ##get the data from the postgres table
+    sql_query = """SELECT taxitype, 
+                EXTRACT(YEAR FROM new_date) as year, LPAD(EXTRACT(MONTH FROM new_date)::text, 2, '0') as month
+                FROM (SELECT  *, DATE((DATE_TRUNC('month', lastmonthprocessed) + INTERVAL '1 month')) AS new_date from nyc) as t"""
+
+    result = postgres_hook.get_records(sql_query
+    )
     # Process the result and save it in a dictionary
     data_dict = {}
     for row in result:
         key = row[0]  # Assuming the first column is the key
-        year = row[1]
-        month = row[2]  # Assuming the second column is the value
-        data_dict[key] = year+"-"+month
+        year = str(row[1])
+        month = str(row[2])
+        data_dict[key]=year+"-"+month
         # context['ti'].xcom_push(key=key, value=value)
-    return data_dict
-with DAG("get_metadata", start_date=datetime(2023, 3 ,14), 
+        print(key, year, month)
+
+
+with DAG(
+    "get_metadata",
+    start_date=datetime(2023, 3, 14),
     schedule_interval="@daily",
-    default_args=default_args, catchup=False,) as dag:
-
-
+    default_args=default_args,
+    catchup=False,
+) as dag:
     nyc_get_audit_data = PythonOperator(
-            task_id="nyc_get_audit_data",
-            python_callable=get_data_postgres, provide_context=True
+        task_id="nyc_get_audit_data",
+        python_callable=get_data_postgres,
+        provide_context=True,
     )
-        
 
     nyc_get_audit_data
