667e569524122ea141a334bec3de602e46aac68d
saurabh sharma <saurabh.sharma@hpe.com>
2023-07-05 13:16:07 -0700

    update airflow code

--- airflow/example_dags/nyc_data.py
+++ airflow/example_dags/nyc_data.py
@@ -1,8 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.contrib.sensors.file_sensor import FileSensor
-from airflow.operators.bash_operator import BashOperator
 from airflow.models import XCom
 from datetime import datetime, timedelta
 import requests
@@ -41,11 +39,13 @@ def get_data_postgres(**context):
 
 
 def download_data_nyc(**context):
-    s3_resource = boto3.resource('s3',
-                        endpoint_url='https://10.10.162.46:9000',
-                        aws_access_key_id='77M1DNDSRUM0WJNJIVUJ5FKCN8E4Y47OGSR7TZPJL49YM878W4T1DEFH8XH4QYJ0YJVAGDNY4OFSCYBYKIWJI1',
-                        aws_secret_access_key='98M280PJIQ7YP2FCXSKQW0ZNSLWU8A4MD20WVQFZ0K55JWVUWJDI',
-                        verify= False)
+    s3_resource = boto3.resource(
+        "s3",
+        endpoint_url="https://10.10.162.46:9000",
+        aws_access_key_id="77M1DNDSRUM0WJNJIVUJ5FKCN8E4Y47OGSR7TZPJL49YM878W4T1DEFH8XH4QYJ0YJVAGDNY4OFSCYBYKIWJI1",
+        aws_secret_access_key="98M280PJIQ7YP2FCXSKQW0ZNSLWU8A4MD20WVQFZ0K55JWVUWJDI",
+        verify=False,
+    )
     for i in ["yellow", "green", "fhv", "fhvhv"]:
         value_taxi = context["ti"].xcom_pull(task_ids="nyc_get_audit_data", key=i)
         url = "https://d37ci6vzurychx.cloudfront.net/trip-data/{}_tripdata_{}.parquet".format(
@@ -55,7 +55,7 @@ def download_data_nyc(**context):
         response = requests.get(url)
         with open(output_file, "wb") as file:
             file.write(response.content)
-        s3_resource.Bucket("dfm").upload_file(output_file,value_taxi+'.parquet')
+        s3_resource.Bucket("dfm").upload_file(output_file, value_taxi + ".parquet")
 
 
 with DAG(
