2c864fa69fd741471effacb96f7c9dec35862db1
saurabh sharma <saurabh.sharma@hpe.com>
2023-07-06 09:49:48 -0700

    connect aws s3 with pyspark

---
+++ python/spark/aws-s3.py
@@ -0,0 +1,23 @@
+## read file from amazon s3
+
+from pyspark.sql import SparkSession
+import logging
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Connect to Amazon S3") \
+    .config("spark.hadoop.fs.s3a.access.key", "AKIAZU27QSEVPZJLRRLP") \
+    .config("spark.hadoop.fs.s3a.secret.key", "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") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
+    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "true") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.hadoop.fs.s3a.region", "us-east-1") \
+    .getOrCreate()
+
+    
+# Set the log level for the Spark application
+spark.sparkContext.setLogLevel("INFO")
+df = spark.read.csv("s3a://ingestion-sprint7/sampleSubmission.csv")
